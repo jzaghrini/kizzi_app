9330be52b3056c7971886af5621394a13002fad8
Zeke Zumbro <jezumbro@gmail.com>
2023-03-27 14:56:22 -0500

    adding back the react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
-
+import react from '@vitejs/plugin-react'
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   return defineConfig({
     root: './src',
     assetsInclude: ['**/*.svg'],
+    plugins: [react()],
     server: {
       watch: {
         usePolling: true,
