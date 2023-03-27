43625325c8fab725209c6fdca470735db51c997a
Zeke Zumbro <jezumbro@gmail.com>
2023-03-27 15:16:48 -0500

    adding application type

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   return defineConfig({
     root: './src',
     assetsInclude: ['**/*.svg'],
     plugins: [react()],
+    appType: 'spa',
     server: {
       watch: {
         usePolling: true,
